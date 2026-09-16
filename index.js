3a5ab100c849cbe2adcf40736470f463756a6378
olesyamakarova1996-oss <olesyamakarova1996@gmail.com>
2026-09-17 00:54:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { JWT } = require('google-auth-library');
 
-const bot = new Telegraf('7922896296:AAFhM1DgwogtgfalIu-M3WDD24-2XavqYxY'); 
+const bot = new Telegraf(process.env.BOT_TOKEN); 
 const CHANNEL_ID = '@phrasal_verbs_ok'; 
 
 // !!! СЮДА ВСТАВЬТЕ ID ВАШЕЙ GOOGLE ТАБЛИЦЫ !!!
